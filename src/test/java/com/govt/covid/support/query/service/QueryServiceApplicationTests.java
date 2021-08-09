b781491c954cb10855d3ae8b5ca5740d174ecd47
athirasabu <athira.sabu@allianz.com>
2021-08-09 12:57:50 +0100

    initial commit

---
+++ src/test/java/com/govt/covid/support/query/service/QueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govt.covid.support.query.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
